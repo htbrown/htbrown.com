fbde6765698f6910598a6f009777ba4a6e2c6e26
Hayden Brown <nedyah.brown@gmail.com>
2021-04-06 12:55:57 +0100

    easter egg

---
+++ assets/js/easterEgg.js
@@ -0,0 +1,6 @@
+body = document.querySelector("body");
+
+function easterEgg() {
+    console.log("easter egg");
+    body.style.backgroundImage = "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,0.5)), linear-gradient(to bottom right, #F10A0A, #F1680A, #F1C70A, #13BF1B, #136CBF, #3A13BF, #BF13A0)"
+}
