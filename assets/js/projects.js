ce8b034a1801e031d2011794b24415531ae0b9f1
Hayden Brown <hayden@htbrown.com>
2022-03-19 23:22:26 +0000

    fix projects and move to directus

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,119 +1,96 @@
 let params = new URLSearchParams(window.location.search);
 
-let projectsArea = document.getElementById("projectsContent");
-let project = "";
+// Bring tags in from CMS for pills
+$.ajax({
+    url: 'https://cms.c.htbrown.net/items/tags',
+    success: (result) => {
+        let tags = "";
+        for (i in result.data) {
+            if (params.get('tag') === result.data[i].name) {
+                tags = tags + '<a href="/projects" class="pill active">' + result.data[i].name + '<i class="pill-close ai-cross"></i></a>';
+            } else {
+                tags = tags + '<a href="/projects?tag=' + result.data[i].name + '" class="pill">' + result.data[i].name + '</a>';
+            }
+        }
+        $('#projectTags').html(tags);
+    },
+    error: () => {
+        $('#projectContent').html('<p><strong>Uh oh.</strong> Data for the requested tag couldn\'t be retrieved. Please try again or <a href="mailto:hayden@htbrown.com">email me</a> if the issue persists.</p>');
+    }
+})
 
-function handleCard(data) {
-  let classname = "card fixed animate__animated animate__zoomIn animate__faster",
-    taglist = "",
-    buttons = "",
-    stylelist = "";
+// Function to build the card object
+async function getCardProps(data) {
+    let classname = "card dark fixed animate__animated animate__zoomIn animate__faster",
+        taglist = "",
+        buttons = "",
+        stylelist = "",
+        featured = "";
 
-  if (data.image != null) {
-    classname = classname + " dark";
-    stylelist =
-      stylelist +
-      "background: linear-gradient( rgba(69, 28, 49, 0.9), rgba(69, 28, 49, 0.9) ), url(https://cms.c.htbrown.net" +
-      data.image.formats.medium.url +
-      ");";
-  }
-  if (data.code != null) {
-    buttons =
-      buttons +
-      '<a href="' +
-      data.code +
-      '" class="btn small"><span><i class="ai-github-fill"></i></span></a>';
-  }
-  if (data.link != null) {
-    buttons =
-      buttons +
-      '<a href="' +
-      data.link +
-      '" class="btn small"><span><i class="ai-link-chain"></i></span></a>';
-  }
-  for (i in data.tags) {
-    if (i != data.tags.length - 1) {
-      taglist = taglist + data.tags[i].name + " &bull; ";
+    if (data.image !== null) stylelist = stylelist + `background: linear-gradient( rgba(69, 28, 49, 0.9), rgba(69, 28, 49, 0.9) ), url(https://cms.c.htbrown.net/assets/${data.image});background-size: cover;`;
+    if (data.code !== null) buttons = buttons + `<a href="${data.code}" class="btn small"><span><i class="ai-github-fill"></i></span></a>`;
+    if (data.link !== null) buttons = buttons + `<a href="${data.link}" class="btn small"><span><i class="ai-link-chain"></i></span></a>`;
+    if (data.featured === true) {
+        featured = '<i class="featured ai-star"></i>';
+        //stylelist = stylelist + 'border: 5px rgb(255, 196, 0) solid;';
+    }
+
+    for (i in data.tags) {
+        if (i != data.tags.length - 1) {
+            taglist = taglist + data.tags[i] + " &bull; ";
+        } else {
+            taglist = taglist + data.tags[i];
+        }
+    }
+    return { classname, taglist, buttons, stylelist, featured };
+}
+
+// Function to handle the data coming in from the CMS
+async function handleData(data) {
+    let projects = "";
+    let featured = "";
+
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+        if (params.get('tag') && (data[i].tags === null || !data[i].tags.includes(params.get('tag')))) continue;
+
+        let properties = await getCardProps(data[i]);
+        let final = `
+            <div class="${properties.classname}" style="${properties.stylelist}">
+                <div class="card-content">
+                    <h2>${data[i].name}${properties.featured}</h2>
+                    <p>${properties.taglist}</p>
+                    <p>${data[i].shortDescription}</p>
+                    <p>${properties.buttons}</p>
+                </div>
+            </div> 
+        `;
+        
+        if (data[i].featured) {
+            featured = featured + final;
+        } else {
+            projects = projects + final;
+        }
+    }
+    if (projects === "") {
+        $('#projectContent').html('<p><strong>Uh oh.</strong> There aren\'t any entries for the chosen tag.</p>');
     } else {
-      taglist = taglist + data.tags[i].name;
+        $('#projectContent').html(projects);
     }
-  }
-  return { classname, taglist, buttons, stylelist };
+    if (featured !== "") {
+        $('#projectFeatured').css('display', 'flex');
+        $('#projectFeatured').html(featured);
+    }
 }
 
-function handleData(data) {
-    if (data.length < 1) {
-        projectsArea.innerHTML = '<p><strong>Uh oh.</strong> There aren\'t any entries for the chosen tag.</p>';
-        return;
+// Get the data from the CMS
+$.ajax({
+    url: 'https://cms.c.htbrown.net/items/projects',
+    success: async (result) => {
+        await handleData(result.data);
+    },
+    error: () => {
+        $('#projectContent').html('<p><strong>Uh oh.</strong> Data for the requested tag couldn\'t be retrieved. Please try again or <a href="mailto:hayden@htbrown.com">email me</a> if the issue persists.</p>');
     }
-  for (i in data) {
-      fetch("https://cms.c.htbrown.net/project/" + data[i].id)
-        .then(r => r.json())
-        .then(async d => {
-            let settings = await handleCard(d);
-                project =
-                  project +
-                  `
-            <div class="${settings.classname}" style="${settings.stylelist}">
-                <h2>${d.name}</h2>
-                <p>${settings.taglist}</p>
-                <p>${d.description}</p>
-                <p>${settings.buttons}</p>
-            </div>   
-          `;
-                projectsArea.innerHTML = project;
-
-        })
-  }
-  project = "", classname = "card fixed", taglist = "", buttons = "", stylelist = "";
-}
-
-if (params.get("tag")) {
-  fetch("https://cms.c.htbrown.net/tags/" + params.get("tag"))
-    .then((r) => r.json())
-    .then(async (data) => {
-      await handleData(data.projects);
-    })
-    .catch((error) => {
-        console.log(error);
-      projectsArea.innerHTML =
-        '<p><strong>Uh oh.</strong> Data for the requested tag couldn\'t be retrieved. Please try again or <a href="mailto:hayden@htbrown.com">email me</a> if the issue persists.</p>';
-    });
-} else {
-  fetch("https://cms.c.htbrown.net/project")
-    .then((r) => r.json())
-    .then(async (data) => {
-      console.log(data);
-      await handleData(data);
-      projectsArea.innerHTML = project;
-    })
-    .catch((error) => {
-        console.log(error);
-      projectsArea.innerHTML =
-        '<p><strong>Uh oh.</strong> Data for the requested tag couldn\'t be retrieved. Please try again or <a href="mailto:hayden@htbrown.com">email me</a> if the issue persists.</p>';
-    });
-}
-
-
-let tagArea = document.getElementById("projectsTags");
-let tags = "";
-
-fetch("https://cms.c.htbrown.net/tags")
-  .then((r) => r.json())
-  .then((data) => {
-    for (i in data) {
-      if (params.get("tag") == data[i].id) {
-        tags =
-          tags + '<a href="/projects" class="pill active">' + data[i].name + "</a>";
-      } else {
-        tags =
-          tags +
-          '<a href="/projects?tag=' +
-          data[i].id +
-          '" class="pill">' +
-          data[i].name +
-          "</a>";
-      }
-    }
-    tagArea.innerHTML = tags;
-  });
+})
